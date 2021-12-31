386a89bb2ff974f3b7134881d45932318fdddd37
David Wilson-Brown <wilsonbrowndavid@gmail.com>
2021-12-30 21:55:46 -0600

    Initial push with errors.  Debugging tool appears as soon as scripts is entered into package.json file

---
+++ README.txt
@@ -0,0 +1,21 @@
+***README***
+*** HOME WORK NOTES ***
+
+// manage my grocery store list
+
+// return the entire list
+    // GET list
+
+//
+    POST /list -d "some item"
+
+// delete the item with at the position
+    DELETE /list/:pos
+
+// update the item at the position
+    PUT /list/:id, body: "some item"
+
+
+// **** THIS DOES NOT WORK UNLESS THE DEBUGGING TOOL IS RUNNING ****
+    // The debug tool pops up immediately when entering in "scripts" in the package.json file
+    // 
---
+++ grocery-app/index.js
@@ -0,0 +1,86 @@
+/**
+ * bring in the express module to use in this app
+ */
+ const express = require("express");
+ // ??? Does this self populate ???
+ const req = require("express/lib/request");
+ 
+ /**
+  * get an app instance using the express framework
+  */
+ const app = express();
+ 
+ app.use(express.json());
+ 
+ /**
+  * define the PORT that the server should be listening on
+  */
+ const PORT = 8080;
+ 
+ /**
+  * create a list, an empty array, meant for items to be shopped
+  * these items should be objects
+  */
+ let list = [];
+ 
+ /**
+  * create an operation to allow for GET REQUESTS to retrieve all items from the list
+  */
+ app.get("/list", function(req, res) {
+     console.log("GET /list");
+     // return the entire list in the response
+     res.json(list);
+ });
+ 
+ /**
+  * create an operation to allow for DELETE REQUESTS on a specific item
+  * delete the item from the list
+  */
+ app.delete("/list/:pos", function(req, res) {
+     console.log("DELETE /list/:id");
+     let deleted = list.splice(req.params.pos, 1);
+     res.json(deleted);
+ })
+ 
+ 
+ /**
+  * create an operation to allow for POST REQUESTS
+  * add an item to the list
+  */
+ app.post("/list", function(req, res) {
+     console.log("POST /list");
+     let input = req.body;
+     // insert the item in json format
+     // ??? Not sure about this ???
+     list.push(input.json());
+     // send a status code showing that POST was successful
+     res.sendStatus(202);
+ 
+ })
+ 
+ 
+ /**
+  * create an operation to allow for PUT REQUESTS
+  * update the item at the position
+  */
+ app.put("/list/:pos", function(req, res) {
+     console.log("PUT /list/:pos");
+     let input = req.body;
+     let pos = req.params.pos;
+     // update the item in json format
+     // ??? Not sure about this ???
+     list(pos) = input.json();
+     // send a status code showing that UPDATE was successful
+     res.sendStatus(202);
+ })
+ 
+ 
+ 
+ 
+ /**
+  * create an operation for the server to be listening on a specified PORT
+  */
+ app.listen(PORT, function() {
+     console.log("This server started and is listening on PORT ", PORT);
+ });
+ 
